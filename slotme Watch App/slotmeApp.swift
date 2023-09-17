306234689a2216731cc89a99d59413a9ca645888
socsob <acanterohio@gmail.com>
2023-09-16 20:17:14 -0400

    Initial Commit

---
+++ slotme Watch App/slotmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotmeApp.swift
+//  slotme Watch App
+//
+//  Created by Andrew Canter on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct slotme_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ slotme/slotmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotmeApp.swift
+//  slotme
+//
+//  Created by Andrew Canter on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct slotmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
